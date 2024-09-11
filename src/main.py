5ebdcc3e1c0b45613d43b31ef3f8e3a699b78bff
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-09-11 14:42:43 +0200

    Added response generation using LLM

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import click
 import yaml
 from src.pdf_to_text.extraction import get_sentences_from_pdf
+from src.llm.run_llm import run_question
 from vector_db.vector_db import create_vector_db
+
 
 @click.command()
 @click.option('--config', '-c', default='../config.yaml', help='Path to the configuration file')
@@ -12,6 +14,7 @@ def run_pipeline(config) -> None:
         pdf_path = config_data.get('pdf_path')  # load path to pdf
         sentences = get_sentences_from_pdf(pdf_path)
         create_vector_db(sentences)
+        run_question("Co wyższe wykształcenie mówi o drugim progu?")
 
 
 if __name__ == "__main__":
