9b18125b2008d4c57fed34bc4cb42d3de4e1c986
Marta <martajjoo9@gmail.com>
2024-09-13 20:55:47 +0200

    GUI version 1.1

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,69 +1,37 @@
 import streamlit as st
 import os
 
-def add_custom_css():
-    st.markdown(
-        f"""
-              <style>
-              /* Ustawienie tła dla całej strony */
-              .stApp {{
-                  background-image: url("https://img.freepik.com/darmowe-wektory/szablon-ramki-niebieski-krzywa_53876-99024.jpg?t=st=1725212108~exp=1725215708~hmac=8ab3e63ad7cecc8747185f54599e74066417b6f5eebe5daaaaebe02b9fd92dd5&w=1060");
-                  background-size: cover;
-                  background-repeat: no-repeat;
-                  background-attachment: fixed;
-              }}
-
-        /* Zmiana stylu tytułu */
-        .stTitle {{
-            color: white;
-            font-size: 3em;
-            text-shadow: 2px 2px 4px #000000;
-        }}
-
-        /* Zmiana stylu pola tekstowego */
-        .stTextInput {{
-            font-size: 1.2em;
-            color: #333;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
 
 def handle_file_upload(path):
     uploaded_file = st.file_uploader("Drop your PDF file here", type="pdf")
+
     if uploaded_file is not None:
-        st.session_state['file_loaded'] = True
         st.session_state['uploaded_file'] = uploaded_file
-        save_path = os.path.join(path, "plik.pdf")
+        save_path = os.path.join(path)
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
+        return 1
     else:
-        st.warning("Uploaded file is empty!")
+        st.warning("No file is uploaded.")
+        return 0
 
 
 def generate_text():
-    user_query = st.session_state.get('user_input', '')
-    if user_query.strip():
-        generated_content = f"Response to: {user_query}"
-        st.session_state['generated_text'] += f"User: {user_query}\nBot: {generated_content}\n\n"
-        st.session_state['user_input'] = ''
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    messages = st.container()
+
+    if prompt := st.chat_input("Say something"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "assistant", "content": f"{prompt}"})
+
+    for message in st.session_state.messages:
+        if message["role"] == "user":
+            messages.chat_message("user").write(message["content"])
+        elif message["role"] == "assistant":
+            messages.chat_message("assistant").write(message["content"])
 
 
 def create_gui(path):
-    add_custom_css()
-
-    st.title("CHATBOX")
-
-    handle_file_upload(path)
-
-    st.text_area("Generated text will appear here", value=st.session_state.get('generated_text', ''), height=450,
-                 key='generated_text_display')
-
-    st.text_area("Enter your text here", height=100,
-                 key='user_input')
-
-    st.button("Send a query", on_click=generate_text)
-
-
+    if handle_file_upload(path) == 1:
+        generate_text()
--- src/main.py
+++ src/main.py
@@ -1,26 +1,19 @@
-import streamlit as st
-from src.gui import app
+from gui import app
 import yaml
 import click
+
 
 def load_config(config_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+
 @click.command()
 @click.option('--config-path', default='config.yaml', help='Path to the .yaml file')
 def main(config_path) -> None:
     config = load_config(config_path)
     path = config['paths']['path_pdf']
-
-    if 'file_name' not in st.session_state:
-        st.session_state['file_name'] = 'No file selected'
-    if 'file_loaded' not in st.session_state:
-        st.session_state['file_loaded'] = False
-    if 'uploaded_file' not in st.session_state:
-        st.session_state['uploaded_file'] = None
-    if 'generated_text' not in st.session_state:
-        st.session_state['generated_text'] = ''
 
     app.create_gui(path)
 
