e980125494b66b4333681a304319dd7fc4f098a7
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-09-30 15:30:51 +0200

    Cleaned code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import click
 import yaml
+from langchain_huggingface import HuggingFaceEmbeddings
 from src.pdf_to_text.extraction import get_sentences_from_pdf
 from vector_db.vector_db import create_vector_db
+
 
 @click.command()
 @click.option('--config', '-c', default='../config.yaml', help='Path to the configuration file')
@@ -10,8 +12,12 @@ def run_pipeline(config) -> None:
         config_data = yaml.safe_load(file)
 
         pdf_path = config_data.get('pdf_path')  # load path to pdf
+        embedding_model_name = config_data.get('embedding_model_name')  # load name of embedding model
+        vector_db_path = config_data.get('vector_db_path')   # load path to save vector db
+
+        embedding_model = HuggingFaceEmbeddings(model_name=embedding_model_name)  # load embedding model
         sentences = get_sentences_from_pdf(pdf_path)
-        create_vector_db(sentences)
+        create_vector_db(sentences, embedding_model, vector_db_path)
 
 
 if __name__ == "__main__":
--- src/vector_db/vector_db.py
+++ src/vector_db/vector_db.py
@@ -1,10 +1,10 @@
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from typing import List
 
 
-def create_vector_db(sentences):
-    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+def create_vector_db(sentences: List[str], embedding_model: HuggingFaceEmbeddings, vector_db_path: str) -> None:
     text_embeddings = embedding_model.embed_documents(sentences)
     text_embedding_pairs = list(zip(sentences, text_embeddings))
     vector_db = FAISS.from_embeddings(text_embedding_pairs, embedding_model)
-    vector_db.save_local("../vector_db_faiss")
+    vector_db.save_local(vector_db_path)
