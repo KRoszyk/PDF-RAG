a3b6459cf1eedc2354255eb87dde524215014c12
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-10-18 17:38:36 +0200

    Cleaned code

--- src/cli.py
+++ src/cli.py
@@ -1,22 +1,25 @@
-from gui import app
 import yaml
 import click
+from interface.app import create_gui
+
 
 def load_config(config_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
+
 @click.command()
 @click.option('--config-path', default='config.yaml', help='Path to the .yaml file')
-def main(config_path) -> None:
-    global created_object  # użycie zmiennej globalnej
-
+def run_pipeline(config_path) -> None:
     config = load_config(config_path)
     path_file = config['paths']['path_pdf']
     path_modified_file = config['paths']['path_modified_pdf']
+    message_container_height = config['message_container_height']
+    layout_division = config['layout_division']
 
-    app.create_gui(path_file, path_modified_file)
+    create_gui(path_file, path_modified_file, message_container_height, layout_division)
+
 
 if __name__ == "__main__":
-    main()
+    run_pipeline()
