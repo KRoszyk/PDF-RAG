bf6efa8c22bee027339eefef519236e735378aa1
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-10-18 17:55:24 +0200

    Changed css

--- src/interface/css.py
+++ src/interface/css.py
@@ -12,5 +12,19 @@ def load_css():
         .st-key-plh1, .st-key-plh2 {
             overflow-x: hidden;
         }
+          .st-emotion-cache-13ln4jf {
+            width: 90%;
+            padding: 3rem 0rem 3rem;
+            max-width: 80rem;
+        }
+        .st-emotion-cache-ocqkz7 {
+            display: flex;
+            flex-wrap: wrap;
+            -webkit-box-flex: 1;
+            flex-grow: 1;
+            -webkit-box-align: stretch;
+            align-items: stretch;
+            gap: 2rem;
+        }
     </style>
     """, unsafe_allow_html=True)
