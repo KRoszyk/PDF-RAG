396bd66774169939714e25e12383055c369437fe
KRoszyk <kamil.roszyk22@gmail.com>
2024-10-25 01:48:22 +0200

    Add new version of GUI with removing uploaded files after clicking the button.

---
+++ src/new_gui/cli.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+import streamlit as st
+
+from session_state import GUIState
+from st_elements import Column1, Column2
+
+
+class GUI(BaseModel):
+    state: GUIState = GUIState()
+    column1: Column1 = Column1(state=state)
+    column2: Column2 = Column2(state=state)
+
+    def show(self):
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            # TODO: Check why this state isn't passed into column1 object (hacked with passing new_state).
+            self.column1.show(new_state=self.state)
+        with col2:
+            self.column2.show()
+
+
+if __name__ == '__main__':
+    st.set_page_config(layout="wide")
+
+    if st.session_state is not None:
+        gui = GUI(state=st.session_state)
+    else:
+        gui = GUI()
+        st.session_state = gui.state.dict()
+
+    gui.show()
+    gui.state.save_state()
---
+++ src/new_gui/session_state.py
@@ -0,0 +1,42 @@
+import random
+import string
+
+from pydantic import BaseModel
+import streamlit as st
+
+
+class PDFUploader(BaseModel):
+    name: str = "Upload PDF file"
+    key: str = ""
+    type: str = "pdf"
+
+    def update_key(self, length: int = 10) -> None:
+        self.key = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+class Button(BaseModel):
+    name: str = ""
+
+class Title(BaseModel):
+    text: str = ""
+
+
+class Column1(BaseModel):
+    title: Title = Title(text="Load file")
+    uploader: PDFUploader = PDFUploader()
+
+
+class Column2(BaseModel):
+    delete_button: Button = Button(name="Delete PDF")
+
+
+class GUIState(BaseModel):
+    col1: Column1 = Column1()
+    col2: Column2 = Column2()
+
+    def save_state(self):
+        st.session_state = self.dict()
+
+    def rerun(self):
+        self.save_state()
+        st.rerun()
