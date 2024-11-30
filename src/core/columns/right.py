a8fef35f58bcf2900d45affc788c59c7c0c88558
Marta <martajjoo9@gmail.com>
2024-11-30 11:38:09 +0100

    Changed name of columns in right side

--- src/core/columns/right.py
+++ src/core/columns/right.py
@@ -16,15 +16,15 @@ class RightColumn:
         next_button_state = self.state.right_col.next_button
         previous_button_state = self.state.right_col.previous_button
         # this _ is for empty space
-        _, left_col, middle_col, _, right_col, _ = st.columns(self.state.layout_right_panel)
+        _, left_text_column, middle_button_column, _, right_button_column, _ = st.columns(self.state.layout_right_panel)
 
-        with left_col:
+        with left_text_column:
             ContentCounter(state=state)
 
-        with middle_col:
+        with middle_button_column:
             st.button(previous_button_state.name, disabled=not uploader_state.is_file_uploaded,
                       use_container_width=True)
-        with right_col:
+        with right_button_column:
             st.button(next_button_state.name, disabled=not uploader_state.is_file_uploaded,
                       use_container_width=True)
 
